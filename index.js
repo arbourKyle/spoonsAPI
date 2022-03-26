31125a1cf87a2c0b8b30e58467b86c7cc6d283f3
KyleA <arbourkyle@gmail.com>
2022-03-26 17:57:56 -0400

    routes functional

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const db = require('./config/connection');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
